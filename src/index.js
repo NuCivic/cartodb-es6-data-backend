101fef3d1ad1d2af1d91ac327d1a706a5becc880
Paul Walker <pjwalker76@gmail.com>
2016-03-15 09:54:45 -0400

    Make url test pass

--- src/index.js
+++ src/index.js
@@ -1,50 +1,60 @@
-const Cartodb = {};
+import * as Es2sql from 'es2sql';
+
+const API_V = 'v2';
+let Cartodb = {};
+let Privates = {};
 
 Cartodb.__type__ = "cartodb";
 
 Cartodb.fetch = function (config) {
-  let url, query, sql;
+  let query, url, sql;
   return new Promise((resolve, reject) => {
 		// parse user and query from url
 		if (config.url) {
+      url = config.url;
 			config.user = privates._parseDatasetUrl(config.url);
 			query = config.url.match(/q=(.*)/g);
 			query = decodeURIComponent(query[0].replace('q=',''));
+			console.log('cdbF0', query);
 		// otherwise create a default query
 		} else {
+      url = Cartodb._getUrl(config);
 			query = config.query || privates._defaultQuery(config);
 			query.table = config.table;
 			query = Es2sql.translate(query);
 		}
-	  url = Cartodb.makeUrl(config, query);
-    fetch(url)
-      .then(res => {
-        console.log('r', res);
-        return res.json();
-      })
-      .then(data => {
-        let output = {};
-        console.log('rr', data);
-        let fields = Object.keys(data.fields).map((val, i) => {
-          return {id: val};
-        });
-        console.log('rrr', fields);
-        output.fields = fields;
-        output.rows = data.rows;
-        output.useMemoryStore = false;
-        console.log('rrrr', output)
-        resolve(output);
-      })
-      .catch(e => {
-        console.log('Error fetching from cartodb', e);
-        reject(e);
-      });
+		//
+    sql.execute(query).done(function (data) {
+			console.log('cdf0.6', data);
+      let output;
+			let fields = _.keys(data.fields).map(function(val, i) {
+				return {id: val};
+			});
+			console.log('cdbf1', fields, config);
+			output = {
+				fields: fields,
+				rows:  data.rows,
+				useMemoryStore: false
+			};
+      resolve(output);
+		});
 	});
 };
 
-Cartodb.makeUrl = function (config, q) {
-  const query = encodeURIComponent(q);
-	return `${config.user}.cartodb.com/api/v2/sql?=${query}`;
+Cartodb._getUrl = function (config) {
+  return `https://${config.user}.cartodb.com/api/${API_V}/sql?`
+};
+
+
+// exports for testing
+Cartodb.Es2sql = Es2sql;
+Cartodb.Privates = Privates;
+
+// Get usermame fron url
+Privates._parseDatasetUrl = function (url) {
+  let s = url.replace(/http(s*):\/\//g, '');
+  s = s.split('.')[0];
+  return s;
 };
 
 export default Cartodb;
--- tests/cartodb_spec.js
+++ tests/cartodb_spec.js
@@ -9,7 +9,46 @@ describe('1+1', () => {
 });
 
 describe('Cartodb module should load', () => {
-  it('Shoukd have a typer', () => {
+  it('Shoukd have a type', () => {
     expect(Cartodb.__type__).toBe('cartodb');
   })
 });
+
+describe('Es2sql module should load', () => {
+  it('Translate function should be a funciton', () => {
+    expect(typeof Cartodb.Es2sql.translate).toBe('function'); // it's a function
+  })
+});
+
+describe('Make URL', () => {
+  it('Should make the right url', () => {
+    let config = {
+      user: 'starsinmypockets',
+      table: 'public.congressional_districts'
+    };
+    const url = Cartodb._getUrl(config);
+    expect(typeof url).toBe('string');
+    expect(url).toBe('https://starsinmypockets.cartodb.com/api/v2/sql?');
+  })
+});
+
+//describe('Should fetch data', () => {
+//  const config = {
+//    user: 'starsinmypockets',
+//    table: 'public.congressional_districts'
+//  }
+//  let result;
+//
+//  beforeEach(() => {
+//    Cartodb.fetch(config)
+//      .then(data => {
+//        console.log('f', data);
+//        result = data;
+//      });
+//  });
+//
+//  it('Should return valid data', () => {
+//    console.log('r', result);
+//    expect(typeof result).toBe('object');
+//  });
+//});
